4b1b6ce1325e5d9ff923a9139d5850c7c3114cae
moshood23 <olaniyanmoshood2017@gmail.com>
2026-08-24 22:15:45 +0100

    feat: add ProdifyDbContext and all entity configurations

---
+++ src/Prodify.Infrastructure/Persistences/Configuration/CartItemConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Prodify.Domain.Cart.Entities;
+
+namespace Prodify.Infrastructure.Persistence.Configurations;
+
+public class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
+{
+    public void Configure(EntityTypeBuilder<CartItem> builder)
+    {
+        builder.ToTable("CartItems");
+
+        builder.HasKey(i => i.Id);
+
+        builder.Property(i => i.CartId)
+            .IsRequired();
+
+        builder.Property(i => i.ProductVariantId)
+            .IsRequired();
+
+        builder.Property(i => i.Quantity)
+            .IsRequired();
+
+        builder.Property(i => i.UnitPrice)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)");
+
+        builder.Ignore(i => i.Subtotal);
+
+        builder.HasIndex(i => i.CartId);
+    }
+}
---
+++ src/Prodify.Infrastructure/Persistences/Configuration/OrderStatusHistoryConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Prodify.Domain.Ordering.Entities;
+
+namespace Prodify.Infrastructure.Persistence.Configurations;
+
+public class OrderStatusHistoryConfiguration : IEntityTypeConfiguration<OrderStatusHistory>
+{
+    public void Configure(EntityTypeBuilder<OrderStatusHistory> builder)
+    {
+        builder.ToTable("OrderStatusHistories");
+
+        builder.HasKey(h => h.Id);
+
+        builder.Property(h => h.SellerOrderId)
+            .IsRequired();
+
+        builder.Property(h => h.Status)
+            .IsRequired()
+            .HasConversion<string>()
+            .HasMaxLength(20);
+
+        builder.Property(h => h.Notes)
+            .HasMaxLength(1000);
+
+        builder.Property(h => h.OccurredAt)
+            .IsRequired();
+
+        builder.HasIndex(h => h.SellerOrderId);
+    }
+}
