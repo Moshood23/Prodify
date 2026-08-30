3a14b71b36c8b500245a1c83f9af686128237fad
moshood23 <olaniyanmoshood2017@gmail.com>
2026-08-30 15:43:30 +0100

    feat: add Inventory module CQRS slices - CreateWarehouse, ReserveStock, GetInventory

---
+++ src/Prodify.Application/Inventory/Commands/ReserveStock/ReserveStockCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Prodify.Application.Inventory.Commands.ReserveStock;
+
+public class ReserveStockCommandValidator : AbstractValidator<ReserveStockCommand>
+{
+    public ReserveStockCommandValidator()
+    {
+        RuleFor(x => x.ProductVariantId)
+            .NotEmpty();
+
+        RuleFor(x => x.WarehouseId)
+            .NotEmpty();
+
+        RuleFor(x => x.Quantity)
+            .GreaterThan(0);
+
+        RuleFor(x => x.ExpiryMinutes)
+            .GreaterThan(0);
+    }
+}
---
+++ src/Prodify.Application/Inventory/Queries/GetInventory/GetInventoryQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Prodify.Application.Common.Exceptions;
+using Prodify.Application.Common.Interfaces;
+
+namespace Prodify.Application.Inventory.Queries.GetInventory;
+
+public class GetInventoryQueryHandler : IRequestHandler<GetInventoryQuery, InventoryDto>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetInventoryQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<InventoryDto> Handle(GetInventoryQuery request, CancellationToken cancellationToken)
+    {
+        var item = await _context.InventoryItems
+            .Where(i => i.ProductVariantId == request.ProductVariantId && i.WarehouseId == request.WarehouseId)
+            .Select(i => new InventoryDto
+            {
+                Id = i.Id,
+                ProductVariantId = i.ProductVariantId,
+                WarehouseId = i.WarehouseId,
+                QuantityOnHand = i.QuantityOnHand,
+                QuantityReserved = i.QuantityReserved,
+                AvailableQuantity = i.AvailableQuantity
+            })
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (item is null)
+            throw new NotFoundException("InventoryItem", $"{request.ProductVariantId}/{request.WarehouseId}");
+
+        return item;
+    }
+}
