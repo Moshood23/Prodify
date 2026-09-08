7079a83987d10ef84b386c02d7f78d334856934e
moshood23 <olaniyanmoshood2017@gmail.com>
2026-09-08 15:36:03 +0100

    Customer

---
+++ src/Prodify.Application/Customers/Commands/RegisterCustomer/RegisterCustomerCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace Prodify.Application.Customers.Commands.RegisterCustomer;
+
+public class RegisterCustomerCommandValidator : AbstractValidator<RegisterCustomerCommand>
+{
+    public RegisterCustomerCommandValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(x => x.LastName)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .MaximumLength(256);
+
+        RuleFor(x => x.PhoneNumber)
+            .MaximumLength(20);
+    }
+}
