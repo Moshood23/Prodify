82d5323cf8437544fc43beb0ae8834768c4568b8
moshood23 <olaniyanmoshood2017@gmail.com>
2026-09-09 10:30:44 +0100

    feat: add Customers module CQRS slices - RegisterCustomer, AddCustomerAddress, GetCustomer

---
+++ src/Prodify.Application/Customers/Queries/GetCustomer/GetCustomerQueryHandler.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Prodify.Application.Common.Exceptions;
+using Prodify.Application.Common.Interfaces;
+
+namespace Prodify.Application.Customers.Queries.GetCustomer;
+
+public class GetCustomerQueryHandler : IRequestHandler<GetCustomerQuery, CustomerDto>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetCustomerQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<CustomerDto> Handle(GetCustomerQuery request, CancellationToken cancellationToken)
+    {
+        var customer = await _context.Customers
+            .Where(c => c.Id == request.Id)
+            .Select(c => new CustomerDto
+            {
+                Id = c.Id,
+                FirstName = c.FirstName,
+                LastName = c.LastName,
+                Email = c.Email,
+                PhoneNumber = c.PhoneNumber,
+                IsActive = c.IsActive,
+                Addresses = c.Addresses.Select(a => new CustomerAddressDto
+                {
+                    Id = a.Id,
+                    Label = a.Label,
+                    RecipientName = a.RecipientName,
+                    AddressLine1 = a.AddressLine1,
+                    AddressLine2 = a.AddressLine2,
+                    City = a.City,
+                    State = a.State,
+                    PostalCode = a.PostalCode,
+                    Country = a.Country,
+                    PhoneNumber = a.PhoneNumber,
+                    IsDefault = a.IsDefault
+                }).ToList()
+            })
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (customer is null)
+            throw new NotFoundException("Customer", request.Id);
+
+        return customer;
+    }
+}
