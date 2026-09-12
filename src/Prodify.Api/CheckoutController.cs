086bf9b72e99fe8381a730c4185801a4bae5b1c7
moshood23 <olaniyanmoshood2017@gmail.com>
2026-09-12 18:09:35 +0100

    feat: add all Api controllers for every module

---
+++ src/Prodify.Api/CheckoutController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Prodify.Application.Ordering.Commands.PlaceOrder;
+
+namespace Prodify.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CheckoutController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CheckoutController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PlaceOrder(PlaceOrderCommand command, CancellationToken cancellationToken)
+    {
+        var orderId = await _mediator.Send(command, cancellationToken);
+        return Ok(orderId);
+    }
+}
---
+++ src/Prodify.Api/CustomersController.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Prodify.Application.Customers.Commands.AddCustomerAddress;
+using Prodify.Application.Customers.Commands.RegisterCustomer;
+using Prodify.Application.Customers.Queries.GetCustomer;
+
+namespace Prodify.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CustomersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CustomersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterCustomerCommand command, CancellationToken cancellationToken)
+    {
+        var id = await _mediator.Send(command, cancellationToken);
+        return CreatedAtAction(nameof(Get), new { id }, id);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> Get(Guid id, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new GetCustomerQuery { Id = id }, cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpPost("{id:guid}/addresses")]
+    public async Task<IActionResult> AddAddress(Guid id, AddCustomerAddressCommand command, CancellationToken cancellationToken)
+    {
+        command.CustomerId = id;
+        var addressId = await _mediator.Send(command, cancellationToken);
+        return Ok(addressId);
+    }
+}
