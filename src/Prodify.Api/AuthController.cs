c68db3b17a6ab374f37a86d8c9a23ea296942c7a
moshood23 <olaniyanmoshood2017@gmail.com>
2026-09-12 13:27:12 +0100

    Controller

---
+++ src/Prodify.Api/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Prodify.Application.Identity.Commands.Login;
+using Prodify.Application.Identity.Commands.Register;
+
+namespace Prodify.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public AuthController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterCommand command, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(command, cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginCommand command, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(command, cancellationToken);
+        return Ok(result);
+    }
+}
