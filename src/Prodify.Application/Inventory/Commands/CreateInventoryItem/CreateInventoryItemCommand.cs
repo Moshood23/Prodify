619cda5a2db4a8c4ffac68ee85ff1e61eb5bb6da
moshood23 <olaniyanmoshood2017@gmail.com>
2026-09-13 12:50:52 +0100

    feat: add CreateInventoryItemCommand and endpoint - closes missing stock creation gap

---
+++ src/Prodify.Application/Inventory/Commands/CreateInventoryItem/CreateInventoryItemCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Prodify.Application.Inventory.Commands.CreateInventoryItem;
+
+public class CreateInventoryItemCommand : IRequest<Guid>
+{
+    public Guid ProductVariantId { get; set; }
+    public Guid WarehouseId { get; set; }
+    public int InitialQuantity { get; set; }
+}
---
+++ src/Prodify.Application/Inventory/Commands/CreateInventoryItem/CreateInventoryItemCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Prodify.Application.Inventory.Commands.CreateInventoryItem;
+
+public class CreateInventoryItemCommandValidator : AbstractValidator<CreateInventoryItemCommand>
+{
+    public CreateInventoryItemCommandValidator()
+    {
+        RuleFor(x => x.ProductVariantId)
+            .NotEmpty();
+
+        RuleFor(x => x.WarehouseId)
+            .NotEmpty();
+
+        RuleFor(x => x.InitialQuantity)
+            .GreaterThanOrEqualTo(0);
+    }
+}
